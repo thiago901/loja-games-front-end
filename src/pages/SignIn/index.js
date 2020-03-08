46acc5ed53401be99c761d304859181ba8e3e9d2
Thiago Santos <tsrocha901@gmail.com>
2020-03-08 00:10:30 -0300

    troca de imagem

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -5,9 +5,9 @@ import { Container, Form, Button } from './style';
 function SignIn() {
   return (
     <Container>
-      <div>
+      <div className="img-left">
         <img
-          src="https://api.thegameawards.com/wp-content/uploads/2019/12/tga-news-featured-1.jpg"
+          src="https://i.pinimg.com/originals/8e/2e/c5/8e2ec5fe56c18e7ec01e8c416c5d450e.jpg"
           alt="Ilustração de Games"
         />
       </div>
